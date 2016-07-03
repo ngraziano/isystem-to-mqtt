5d206f98cf33a7cf5bde05bf2e7a945b358f0f34
Nicolas Graziano <nicolas.graziano@gmail.com>
2016-07-03 19:29:49 +0200

    Add a setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='isystem-to-mqtt',
+    version='0.1.0',
+    author='Mickael Le Baillif',
+    author_email='nicolas.graziano+py@gmail.com',
+    packages=['isystem-tom-qtt'],
+    scripts=['bin/poll_isystem_mqtt.py'],
+    url='https://github.com/ngraziano/isystem-to-mqtt',
+#    license='LICENSE.txt',
+    description='Request Isystem boiler and send value to mqtt',
+#    long_description=open('README.md').read(),
+)
