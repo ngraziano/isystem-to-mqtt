f749227a1a6295062534d4be3de5d4a9b824c279
Nicolas Graziano <nicolas.graziano@engie.com>
2016-07-09 18:24:34 +0200

    Multiple publish for value.

--- isystem_to_mqtt/tables.py
+++ isystem_to_mqtt/tables.py
@@ -1,4 +1,4 @@
-from .tag_definition import TagDefinition, WriteTagDefinition
+from .tag_definition import TagDefinition, WriteTagDefinition, MultipleTagDefinition
 from . import convert
 
 READ_TABLE = {
@@ -15,17 +15,19 @@ READ_TABLE = {
     650: TagDefinition("zone-a/day-target-temperature", convert.tenth),
     651: TagDefinition("zone-a/night-target-temperature", convert.tenth),
     652: TagDefinition("zone-a/antifreeze-target-temperature", convert.tenth),
-    653: TagDefinition("zone-a/mode", convert.derog_bit),
+    653: MultipleTagDefinition([("zone-a/mode", convert.derog_bit),
+                                ("zone-a/mode-raw", convert.unit)]),
     654: TagDefinition("zone-a/sensor-influence", convert.unit),
     655: TagDefinition("zone-a/curve", convert.tenth),
     656: TagDefinition("zone-b/day-target-temperature", convert.tenth),
     657: TagDefinition("zone-b/night-target-temperature", convert.tenth),
     658: TagDefinition("zone-b/antifreeze-target-temperature", convert.tenth),
-    659: TagDefinition("zone-b/mode", convert.derog_bit)
+    659: MultipleTagDefinition([("zone-b/mode", convert.derog_bit),
+                                ("zone-b/mode-raw", convert.unit)])
 }
 
 WRITE_TABLE = {
-    "zone-a/program/SET" : WriteTagDefinition(231, convert.write_unit), 
-    "zone-a/day-target-temperature/SET" : WriteTagDefinition(650, convert.write_tenth),
-    "zone-a/night-target-temperature/SET" : WriteTagDefinition(651, convert.write_tenth) 
+    "zone-a/program/SET": WriteTagDefinition(231, convert.write_unit),
+    "zone-a/day-target-temperature/SET": WriteTagDefinition(650, convert.write_tenth),
+    "zone-a/night-target-temperature/SET": WriteTagDefinition(651, convert.write_tenth)
 }
--- isystem_to_mqtt/tag_definition.py
+++ isystem_to_mqtt/tag_definition.py
@@ -14,8 +14,22 @@ class TagDefinition(object):
     def publish(self, client, base_topic, raw_values, index):
         """ Publish the converted value to mqtt using client parameter"""
         value = self.convertion(raw_values, index)
-        _LOGGER.debug("value %s  = %s",self.tag_name, value)
+        _LOGGER.debug("value %s  = %s", self.tag_name, value)
         client.publish(base_topic + self.tag_name, value, retain=True)
+
+class MultipleTagDefinition(object):
+    """ Define a tag with multiple mqtt topic and convertion """
+
+    def __init__(self, definition_list, needed_value=1):
+        self.definition_list = definition_list
+        self.needed_value = needed_value
+
+    def publish(self, client, base_topic, raw_values, index):
+        """ Publish the converted value to mqtt using client parameter"""
+        for (convertion,tag_name) in self.definition_list:
+            value = convertion(raw_values, index)
+            _LOGGER.debug("value %s  = %s", tag_name, value)
+            client.publish(base_topic + tag_name, value, retain=True)
 
 
 class WriteTagDefinition(object):
