53b031f5b4b8f98f396dbe9259b256e9891b1309
Nicolas Graziano <nicolas.graziano@gmail.com>
2016-12-03 16:19:43 +0100

    Add needed future.

--- isystem_to_mqtt/convert.py
+++ isystem_to_mqtt/convert.py
@@ -1,5 +1,10 @@
 """ Function to convert raw modbus value """
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import datetime
 import json
 from . import time_delta_json
